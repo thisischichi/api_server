fd3f6afd1fa9c7ebe9b41e94604a422cbf89f213
Chi-chi <thisischichi@gmail.com>
2019-06-03 20:51:26 +0100

    require 'cors'

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const app = express()
 const port = 3010
 const db = require('./queries')
 
+app.use(cors())
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
